7e7360cb81d1995843a54bfe52b42df1aef427d9
Adi Gunawan Lase <adiglase6@gmail.com>
2023-06-09 22:53:40 +0700

    Add more input field in register page

--- src/pages/register/RegisterPage.vue
+++ src/pages/register/RegisterPage.vue
@@ -38,6 +38,33 @@
           :rules="[val => !!val || 'Field is required']"
         />
 
+        <q-select
+          outlined
+          v-model="gender"
+          :options="genderOptions"
+          label="Gender"
+          stack-label
+          hide-bottom-space
+          :rules="[val => !!val || 'Field is required']"
+        />
+
+        <q-input outlined v-model="dateOfBirth"
+                 label="Date of Birth" stack-label
+                 hide-bottom-space
+                 mask="date" :rules="[date]">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                <q-date format-date="DD-MM-YYYY" v-model="dateOfBirth">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat/>
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+
         <q-input
           outlined
           v-model="password"
@@ -68,6 +95,7 @@
             rounded
             no-caps
             padding="md"
+            onsubmit="onSubmit"
           />
         </div>
       </q-form>
@@ -80,20 +108,35 @@
   </div>
 </template>
 <script setup>
-import { useQuasar } from 'quasar';
+import { date, useQuasar } from 'quasar';
 import { ref } from 'vue';
 
 const $q = useQuasar();
 
+const genderOptions = [
+  {
+    label: 'Male',
+    value: 0,
+  },
+  {
+    label: 'Female',
+    value: 1,
+  },
+];
+
 const fullName = ref(null);
 const username = ref(null);
 const email = ref(null);
+const gender = ref(null);
 const dateOfBirth = ref(null);
-const gender = ref(0);
 const password = ref(null);
 const password2 = ref(null);
 
 const isLoading = ref(false);
+
+function onSubmit() {
+  return 'test';
+}
 </script>
 <style lang="scss" scoped>
 #register-page {
@@ -102,16 +145,19 @@ const isLoading = ref(false);
   flex-direction: column;
   justify-content: center;
 }
+
 h1 {
   font-size: 30px;
   line-height: 1;
   margin: 12px 0;
 }
+
 h3 {
   font-size: 14px;
   color: $nero-mid;
   line-height: 1;
 }
+
 #register-form {
   margin-top: 20px;
 }
